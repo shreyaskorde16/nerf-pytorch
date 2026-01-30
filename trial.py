007cfaa4daaf04090b4f332cfd7a370c496db1de
shreyaskorde16 <shreyaskorde16@gmail.com>
2026-01-30 13:38:43 +0100

    feat: rectangle added

--- trial.py
+++ trial.py
@@ -4,7 +4,7 @@ print(torch.__version__)  # Should output 1.11.0 or 2.3.0
 print(torch.cuda.is_available())  # Should output True
 print(torch.version.cuda)  # 11.3
 
-object = 'square' # enter object name
+object = 'rectangle' # enter object name
 
 split = ["train", "val", "test"]
 
