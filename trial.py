efc94af688c4cf4b139500917a0370808154ddf9
shreyaskorde16 <shreyaskorde16@gmail.com>
2026-02-03 16:27:09 +0100

    feat: cylider data updates ad configs

--- trial.py
+++ trial.py
@@ -4,7 +4,7 @@ print(torch.__version__)  # Should output 1.11.0 or 2.3.0
 print(torch.cuda.is_available())  # Should output True
 print(torch.version.cuda)  # 11.3
 
-folder_name = 'square_update_v0.1' # enter object name
+folder_name = 'cylinder_update_v0.1' # enter object name
 
 split = ["train", "val", "test"]
 
